8bc6921ab30a9abcd203ec4a21ade483fa74aaf7
Brian Donohue <brian.donohue89@gmail.com>
2014-05-10 10:17:12 -0400

    Initial Commit

---
+++ WhereBrianAt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WhereBrianAt
+//
+//  Created by Brian Donohue on 5/10/14.
+//  Copyright (c) 2014 Brian Donohue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ WhereBrianAtTests/WhereBrianAtTests.m
@@ -0,0 +1,34 @@
+//
+//  WhereBrianAtTests.m
+//  WhereBrianAtTests
+//
+//  Created by Brian Donohue on 5/10/14.
+//  Copyright (c) 2014 Brian Donohue. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WhereBrianAtTests : XCTestCase
+
+@end
+
+@implementation WhereBrianAtTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
